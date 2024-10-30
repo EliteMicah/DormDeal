dad0bae24b424b43f388c25a19f568f734cfd9f3
Micah Woodring <micahkwoodring@gmail.com>
2024-10-30 11:30:13 -0700

    Working on resources button

--- app/(tabs)/homeScreen.tsx
+++ app/(tabs)/homeScreen.tsx
@@ -1,12 +1,25 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Text, View } from "@/components/Themed";
 import "../../global.css";
 
-export default function homeScreen() {
+export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-blue-700 font-extrabold">Rebooked</Text>
-    </View>
+    <SafeAreaView className="h-screen flex w-screen">
+      <View className="w-full pt-[1vh] flex-1 items-center border-2 border-red-500">
+        <Text style={{ color: "#38b6ff" }} className="font-extrabold text-4xl">
+          Rebooked
+        </Text>
+      </View>
+      <View className="w-full flex-1 justify start border-2 border-blue-500">
+        <Button
+          title="Resources!"
+          onPress={() => {
+            console.log("Button pressed!");
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
