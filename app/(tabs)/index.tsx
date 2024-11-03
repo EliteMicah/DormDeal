99b63bfe6fc58799c9be094cff873616930545e4
Micah Woodring <micahkwoodring@gmail.com>
2024-11-02 17:43:01 -0700

    Started working with cards and shadows

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { Button, StyleSheet, TouchableOpacity } from "react-native";
+import { Button, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text, View } from "@/components/Themed";
-// import Card from "react-bootstrap/Card";
 import "../../global.css";
-//import "bootstrap/dist/css/bootstrap.min.css";
+
+// const { width: deviceWidth } = Dimensions.get("window");
+const deviceWidth = Math.round(Dimensions.get("window").width);
 
 export default function HomeScreen() {
   return (
@@ -12,10 +13,8 @@ export default function HomeScreen() {
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Rebooked</Text>
       </View>
+      <View style={styles.cardContainer}></View>
       <View style={styles.separator}></View>
-      {/* <Card style={{ width: "18rem" }}>
-        <Card.body></Card.body>
-      </Card> */}
       <View style={styles.resourcesContainer}>
         <TouchableOpacity
           onPress={() => {
@@ -32,22 +31,44 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   maincontainer: {
     flex: 1,
-    width: "100%",
+    width: deviceWidth,
     height: "100%",
     backgroundColor: "#f2f2f2",
+    // Main container does not go past bottom navigation bar
   },
   titleContainer: {
     alignItems: "center",
     width: "100%",
     backgroundColor: "#f2f2f2",
+    marginVertical: 10,
+    marginBottom: 40,
   },
   title: {
     fontSize: 35,
     fontWeight: "800",
     color: "#38b6ff",
+    shadowColor: "#aaa",
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.5,
     //borderWidth: 2,
     //borderRadius: 10,
     //borderColor: "black",
+  },
+  cardContainer: {
+    //width: deviceWidth - 25,
+    backgroundColor: "#aaaaaa",
+    height: 200,
+    borderRadius: 10,
+    marginHorizontal: 30,
+    shadowColor: "#aaa",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.75,
   },
   resourcesContainer: {
     alignItems: "center",
@@ -58,6 +79,12 @@ const styles = StyleSheet.create({
     borderColor: "black",
     marginTop: 20,
     alignSelf: "center",
+    shadowColor: "#aaa",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.75,
   },
   resourcesButton: {
     fontSize: 25,
@@ -69,8 +96,6 @@ const styles = StyleSheet.create({
     margin: 8,
   },
   separator: {
-    marginVertical: 100,
-    height: 1,
-    width: "80%",
+    marginVertical: 170,
   },
 });
